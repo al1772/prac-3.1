8e200ee3e35aee5c54a4c6537a0b79c04ea0b8a1
Alicia <129069621+al1772@users.noreply.github.com>
2024-12-19 13:07:12 +0000

    19.12

--- Assets/launchScene.cs
+++ Assets/launchScene.cs
@@ -1,24 +1,30 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class launchScene : MonoBehaviour
 {
     // Start is called before the first frame update
-    public string game2;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void OnTriggerEnter(Collider FPSController){
-        Debug.Log("loading");
-        // SceneManager.LoadScene(game2);
+    void OnTriggerStay(Collider FPSController){
+        if (Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("loading new scene");
+            SceneManager.LoadScene(1);
+        }
+
     }
 }
